f79e0bae5212fa21ef9c18152ed346e2dc764d62
abarna10303 <abarnas361@gmail.com>
2022-12-15 22:03:50 +0530

    Even

---
+++ main.js
@@ -0,0 +1,21 @@
+var num=prompt("Enter the Number");
+var i=1;
+do
+{
+    if(i%2==0)
+    {
+        document.write(i);
+        
+    }
+    document.write("<br>");
+        i++;
+}while(i<=num)
+/*
+for(i=1;i<=num;i++)
+{
+    if(i%2==0)
+    {
+        document.write(i);
+    }
+    document.write("<br>");
+}*/
